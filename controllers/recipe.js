4dc1931644741a583656c1863e8a6181ceba7f70
Glorius King <gloriusking3@gmail.com>
2024-07-14 12:00:40 +0200

    updated recipe.js

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -12,14 +12,36 @@ const recipeControllers = {
     getOneRecipe: async (req, res) => {
         const { id } = req.params;
         try {
-            
+            const output = await query('SELECT * FROM recipes WHERE id = $1');
+            res.status(200).json(output);
         } catch (error) {
             res.status(500).json({error: 'Failed to retrieve the recipe'});
         }
     },
-    postRecipe: async (req, res) => {},
-    updateRecipe: async (req, res) => {},
-    deleteRecipe: async (req, res) => {},
+    postRecipe: async (req, res) => {
+        const { title, totalYield, ingredients, instructions } = req.body;
+        try {
+            
+        } catch (error) {
+            res.status(500).json({error: 'Failed to create a new recipe'});
+        }
+    },
+    updateRecipe: async (req, res) => {
+        const { id } = req.params;
+        try {
+            
+        } catch (error) {
+            res.status(500).json({error: 'Failed to update the recipe'});
+        }
+    },
+    deleteRecipe: async (req, res) => {
+        const { id } = req.params;
+        try {
+            
+        } catch (error) {
+            res.status(500).json({error: 'Failed to delete recipe'});
+        }
+    },
 };
 
 export default recipeControllers;
